38ef98b9c546803007d835ce154467739b59ef59
sagarb1 <sagar11105@gmail.com>
2019-10-16 16:16:04 +0530

    added Junit Mokito-test 2

---
+++ src/test/java/com/sagar/spring5recipeapp/controller/IndexControllerTest.java
@@ -0,0 +1,42 @@
+package com.sagar.spring5recipeapp.controller;
+
+import com.sagar.spring5recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * created by sagar on 16-10-2019
+ */
+
+public class IndexControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+
+        String viewName = controller.getIndexPage(model);
+        //then
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
